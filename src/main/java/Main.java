cfc937b493e30357912def1f6ca7c3e841a5ba43
Ildrassil <schneiderjakob7@googlemail.com>
2023-12-14 11:24:38 +0100

    new Customer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         CustomerRepo customerRepo = new CustomerRepo();
         customerRepo.creatCustomer(1,"Sviat","Schneider","123@test.de","017612783732","123 Street","Chicago","Ilinois","87878","United States");
-
+        customerRepo.creatCustomer(2,"Sviat","Schneider","123@test.de","017612783732","123 Street","Chicago","Ilinois","87878","United States");
 
 
     }
