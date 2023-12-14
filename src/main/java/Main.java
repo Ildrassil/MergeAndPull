da6366f3ca0b9d86be5dce86f7ea4d32779ac0de
Ildrassil <schneiderjakob7@googlemail.com>
2023-12-14 11:22:45 +0100

    Add-Feature: getAllCustomers

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,10 @@ public class Main {
     public static void main(String[] args) {
         CustomerRepo customerRepo = new CustomerRepo();
         customerRepo.creatCustomer(1,"Sviat","Schneider","123@test.de","017612783732","123 Street","Chicago","Ilinois","87878","United States");
-
+        customerRepo.creatCustomer(2,"Jakob","Peter","897@mail.de","134614981","546 Street","Leipzig","Sachsen","30786","Deutschland");
+        customerRepo.creatCustomer(3,"Peter","Lisa","675@mail.de","05126939231","789 Street","Berlin","Berlin","10156","Deutschland");
+        customerRepo.creatCustomer(4,"Lisa","Schmidt","55448@mail.de","446846416","155 Street","Berlin","Berlin","10156","Deutschland");
+        System.out.println(customerRepo.getAllCustomers());
 
 
     }
